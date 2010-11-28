3c3ed2909619ae6eb9f79a93c1582239a004691e
DonAndy <donandy01@googlemail.com>
2010-11-28 14:40:00 +0100

    Removed wrongly placed medkit

--- entities/item_tjp_health/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type = "anim"
-
-ENT.PrintName = "TJP Healthkit"
-ENT.Author = "Don Andy"
-ENT.Purpose = "It's a healthkit derp"
-ENT.Instructions = ""
-ENT.Spawnable = false
-ENT.AdminSpawnable = true
